d0fc9efa621d44c6a19ef2961a2eea371a482a31
Dane Orie <dane.orie@gmail.com>
2013-12-24 11:28:53 -0500

    Testing Code under *Test directory

---
+++ *Test/Roll-a-ball/Assets/Scripts/CardController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardController : MonoBehaviour {
+
+	void OnMouseDrag() {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit)) {
+			transform.position = new Vector3(hit.point.x, 0.25f, hit.point.z);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		GameObject obj = collision.gameObject;
+		Transform target = obj.transform;
+		Vector3 a = transform.rotation.eulerAngles;
+		Vector3 b = target.rotation.eulerAngles;
+		bool sameObject = tag == obj.tag;
+		bool flat = a.x < 10 && b.x < 10;
+		bool nearby = Vector3.Distance(a,b) < 500;
+		print(Vector3.Distance(a,b));
+		if (sameObject && flat && nearby) {
+			if (transform.position.y > target.position.y) {
+				transform.position = new Vector3(
+					target.position.x,
+					target.position.y + 0.06f,
+					target.position.z
+				);
+				transform.rotation = target.rotation;
+			}
+		}
+	}
+
+}
---
+++ *Test/Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	public GUIText countText;
+	public GUIText winText;
+	private int count;
+	
+	void Start() {
+		setCountText(count = 0);
+		winText.text = "";
+	}
+		
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		
+		rigidbody.AddForce(movement * speed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive(false);
+			count++;
+			setCountText(count);
+		}
+	}
+	
+	void setCountText(int count) {
+		countText.text = "Count: " + count;
+		if (count >= 4) {
+			winText.text = "Winner!";
+			this.gameObject.SetActive(false);
+		}
+	}
+
+}
